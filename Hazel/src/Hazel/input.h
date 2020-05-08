7a22f3c680442a35f872537211cfa4663ed23835
Devin James <devsch1@gmail.com>
2020-05-08 14:08:56 -0500

    added a system for us to ask hazel about event states, like key presses or such

---
+++ Hazel/src/Hazel/input.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <Hazel/Core.h>
+
+namespace Hazel {
+
+	class HAZEL_API Input
+	{
+	public:
+		inline static bool IsKeyPressed(int keyCode) { return s_Instance->IsKeyPressedImpl(keyCode); }
+
+		inline static bool IsMouseButtonPressed(int button) { return s_Instance->IsMouseButtonPressedImpl(button); }
+		inline static double GetMouseX() { return s_Instance->GetMouseXImpl(); }
+		inline static double GetMouseY() { return s_Instance->GetMouseYImpl(); }
+		inline static std::pair<double, double> GetMousePos() { return s_Instance->GetMousePosImpl(); }
+
+	protected:
+		virtual bool IsKeyPressedImpl(int keyCode) = 0;
+
+		virtual bool IsMouseButtonPressedImpl(int button) = 0;
+		virtual double GetMouseXImpl() = 0;
+		virtual double GetMouseYImpl() = 0;
+		virtual std::pair<double, double> GetMousePosImpl() = 0;
+	private:
+		static Input* s_Instance;
+	};
+
+}
