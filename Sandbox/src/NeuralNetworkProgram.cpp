ff4a3285df0c2000084d80c54cc183ca2090222f
Devin James <devsch1@gmail.com>
2021-11-07 16:41:34 -0600

    improved ui for neural network

--- Sandbox/src/NeuralNetworkProgram.cpp
+++ Sandbox/src/NeuralNetworkProgram.cpp
@@ -3,6 +3,7 @@
 #include <random>
 
 #include "imgui/imgui.h"
+#include <imgui/imgui_internal.h>
 
 class NeuralNetworkLayer : public Hazel::Layer
 {
@@ -13,9 +14,14 @@ public:
 		nn = network(layers);
 		data.reserve(16);
 		data.push_back({ {0, 0}, {0, 0} });
-		data.push_back({ {0, 1}, {1, 1} });
-		data.push_back({ {1, 0}, {1, 1} });
-		data.push_back({ {1, 1}, {0, 1} });
+		data.push_back({ {0, 1}, {0, .5} });
+		data.push_back({ {1, 0}, {0, 1} });
+		data.push_back({ {1, 1}, {0, 0} });
+		data.push_back({ {.25, .25}, {1, .5} });
+		data.push_back({ {.25, .75}, {1, 1} });
+		data.push_back({ {.75, .25}, {1, 0} });
+		data.push_back({ {.75, .75}, {1, .5} });
+		data.push_back({ {.5, .5}, {0, 1} });
 
 	}
 
@@ -40,21 +46,61 @@ public:
 
 
 		Hazel::Renderer2D::ResetStats();
-		//Hazel::Renderer2D::BeginScene(m_Camera.GetCamera());
-		//
-		//float delta = .01;
-		//for (float i = 0; i <= 1; i+=delta)
-		//{
-		//	for (float j = 0; j <= 1; j+=delta)
-		//	{
-		//		Hazel::Renderer2D::DrawQuad({ i * 2 - 1, j * 2 - 1 }, { delta*.98, delta*.98 }, { 1, 0, 1, 1 });
-		//		Hazel::Renderer2D::DrawQuad({ 1, 0 }, { 1, 1 }, { 1, 0, 1, 1 });
-		//		if(j == 1 && i == 1)
-		//			HZ_CORE_INFO("{0}\t{1}", i * 4 - 1, j * 2 - 1);
-		//	}
-		//}
-		//
-		//Hazel::Renderer2D::EndScene();
+
+
+		Hazel::Renderer2D::BeginScene(m_Camera.GetCamera());
+
+		float delta0 = 1 / (float)(delta - 1);
+		for (float i = 0; i <= 1.00001; i += delta0)
+		{
+			for (float j = 0; j <= 1.00001; j += delta0)
+			{
+				glm::vec2 pos = { i, j };
+				auto output = nn.input({ i, j });
+				Hazel::Color result(0);
+
+				if (toggle == 0)
+				{
+					result += Hazel::Color(output.x, 0, 0);
+				}
+				else if (toggle == 1)
+				{
+					result += Hazel::Color(0, 0, output.y);
+				}
+				else
+					result += Hazel::Color(output.x, 0, output.y);
+
+
+				for (auto& targets : data)
+				{
+					auto posi = targets.input;
+					if (posi.x > pos.x - delta0 / 2.f && posi.x <= pos.x + delta0 / 2.f && posi.y > pos.y - delta0 / 2.f && posi.y <= pos.y + delta0 / 2.f)
+					{
+						if (showTargets)
+						{
+							if (toggle == 0) { result = Hazel::Color(targets.output.x, 0, 0); }
+							else if (toggle == 1)
+							{
+								result = Hazel::Color(0, 0, targets.output.y);
+							}
+							else
+								result = Hazel::Color(targets.output.x, 0, targets.output.y);
+
+
+							break;
+						}
+						result += Hazel::Color(0, 1, 0);
+						break;
+					}
+				}
+
+				Hazel::Renderer2D::DrawQuad(glm::vec2(map(i, 0, 1, -(1 - delta0), (1 - delta0)), map(j, 0, 1, -(1 - delta0), (1 - delta0)))*.99f, 2.f * glm::vec2(delta0, delta0), result);
+				result = Hazel::Color(0);
+			}
+		}
+		Hazel::Renderer2D::EndScene();
+
+
 		timeStep = ts.GetSeconds();
 	}
 
@@ -68,41 +114,49 @@ public:
 			ImGui::Begin("Settings");
 
 
-			Hazel::Renderer2D::BeginScene(m_Camera.GetCamera());
-
-			static int delta = 50;
-			float delta0 = 1 / (float)delta;
-			for (float i = 0; i <= 1; i += delta0)
-			{
-				for (float j = 0; j <= 1; j += delta0)
-				{
-					auto output = nn.input({ i, j });
-					Hazel::Color result;
-					if (toggle == 0)
-					{
-						result = Hazel::Color(output.x, 0, 0);
-					}
-					else if (toggle == 1)
-					{
-						result = Hazel::Color(0, 0, output.y);
-					}
-					else
-						result = Hazel::Color(output.x, 0, output.y, 1);
-					Hazel::Renderer2D::DrawQuad({ i * 2 - 1 + delta0 / 2.f, j * 2 - 1 + delta0 / 2.f }, { delta0 * 2, delta0 * 2 }, result);
-				}
-			}
-
-			Hazel::Renderer2D::EndScene();
-
 			auto stats = Hazel::Renderer2D::GetStats();
 			ImGui::Text("draw calls: %d", stats.drawCalls);
 			ImGui::Text("\nFrame Rate: %f, frameTime: %f", 1 / timeStep, timeStep);
 			int index = 0;
-			for (auto& points : data)
+
+
+
+
+			if (data.size() != 0)
 			{
-				ImGui::SliderFloat4(("point#" + std::to_string(index++)).c_str(), &points.input.x, 0, 1);
-				ImGui::Text("\tError: %f, %f", points.error(nn).x, points.error(nn).y);
+				const ImGuiTreeNodeFlags treeNodeFlags = ImGuiTreeNodeFlags_Framed | ImGuiTreeNodeFlags_SpanAvailWidth |
+					ImGuiTreeNodeFlags_AllowItemOverlap | ImGuiTreeNodeFlags_FramePadding;
+				ImVec2 contentRegionAvailable = ImGui::GetContentRegionAvail();
+
+				ImGui::PushStyleVar(ImGuiStyleVar_FramePadding, ImVec2{ 4, 4 });
+				float lineHeight = GImGui->Font->FontSize + GImGui->Style.FramePadding.y * 2.0f;
+				ImGui::Separator();
+				bool open = ImGui::TreeNodeEx((void*)&data, treeNodeFlags, "Training Data");
+				ImGui::PopStyleVar();
+
+				if (open)
+				{
+					for (int i = 0; i < data.size(); i++)
+					{
+						auto& points = data[i];
+						ImGui::SliderFloat4(("point#" + std::to_string(index++)).c_str(), &points.input.x, 0, 1);
+						ImGui::Text("\tError: %f, %f", points.error(nn).x, points.error(nn).y);
+						ImGui::SameLine();
+						if (ImGui::Button(std::string("remove Data Point##" + i).c_str()))
+						{
+							data.erase(data.begin() + i);
+						}
+					}
+					ImGui::TreePop();
+				}
+
+
 			}
+
+
+
+
+
 			static glm::vec2 newPoints[2] = { {.5, .5}, {.5, .5} };
 			ImGui::DragFloat4("new point", &newPoints[0].x, .01f, 0, 1);
 			ImGui::SameLine();
@@ -165,8 +219,29 @@ public:
 
 			ImGui::SameLine();
 			ImGui::Checkbox("training", &train);
+			ImGui::Checkbox("show what target values are suppose to be?", &showTargets);
 
-				ImGui::Text("Network Values:\n\n%s", nn.to_string().c_str());
+
+			static std::string funcString[] = { "Sigmoid", "Tanh", "LeakyReLU", "Elu", "Swish", "SoftMax", "Linear"};
+			auto selectedfunc = funcString[currentFunc];
+			if (ImGui::BeginCombo("Activation Function", selectedfunc.c_str()))
+			{
+				for (int i = 0; i < 7; i++)
+				{
+					bool isSelected = selectedfunc == funcString[i];
+					if (ImGui::Selectable(funcString[i].c_str(), isSelected))
+					{
+						currentFunc = (Activationfunctions)i;
+					}
+
+					if (isSelected)
+						ImGui::SetItemDefaultFocus();
+				}
+
+				ImGui::EndCombo();
+			}
+			ImGui::DragFloat("learningRate", &nn.learningRate, .001, .001, 1.001);
+			ImGui::Text("Network Values:\n\n%s", nn.to_string().c_str());
 
 			ImGui::End();
 		}
@@ -182,11 +257,12 @@ public:
 private:
 	Hazel::OrthographicCameraController m_Camera;
 	network nn;
-	int layers = 1;
+	int layers = 1, delta = 50;
 
 	float timeStep = 0.f;
 	int toggle = 0;
 	bool train = true;
+	bool showTargets = false;
 
 	Hazel::Ref<Hazel::Framebuffer> m_FrameBuffer;
 	std::vector<TrainingData> data;
